e4b9ab4662323e81006f45147bb21a384d2ac3ff
kyawrkar13-sys <kyawrkar13@gmail.com>
2026-07-14 14:30:47 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+
+    return render_template("index.html")
+
+@app.route("/solar")
+
+def solar():
+
+    return render_template("solar.html")
+
+@app.route("/generator")
+
+def generator():
+
+    return render_template("generator.html")
+
+if __name__ == "__main__":
+
+    app.run(host="0.0.0.0", port=5000)
