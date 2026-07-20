d2a761f8cb487272cb65fa88ccf7d9331337f011
kyawrkar13-sys <kyawrkar13@gmail.com>
2026-07-21 04:49:16 +0630

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,39 @@
+const CACHE_NAME = "solar-roi-v1";
 
+const files = [
+"/",
+"/style.css",
+"/icon.png"
+];
+
+
+self.addEventListener("install", e=>{
+
+e.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache=>{
+
+return cache.addAll(files);
+
+})
+
+);
+
+});
+
+
+self.addEventListener("fetch", e=>{
+
+e.respondWith(
+
+caches.match(e.request)
+.then(response=>{
+
+return response || fetch(e.request);
+
+})
+
+);
+
+});
